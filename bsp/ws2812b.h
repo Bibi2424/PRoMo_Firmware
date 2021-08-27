ba5cb998f891f87070f25d11f838c9d6245eb677
Bibi <guillaume.dib@gmail.com>
2021-08-27 19:41:00 -0400

    reworked bitbang and add SPI implementation

--- bsp/ws2812b.h
+++ bsp/ws2812b.h
@@ -4,11 +4,18 @@
 #include <stdint.h>
 #include <stdbool.h>
 
+
+#define USE_DUMMY_IMPLEMENTATION	0
+#define USE_SPI_IMPLEMENTATION		1
+#define WS2812B_DRIVE_METHOD		USE_DUMMY_IMPLEMENTATION
+
+
 typedef struct {
 	uint8_t r;
 	uint8_t g;
 	uint8_t b;
 } rgb_t;
+
 
 
 extern void ws2812b_init(void);
