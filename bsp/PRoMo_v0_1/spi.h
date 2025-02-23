b7e64a829821960250979d2c7ffa22dd068bc878
Bibi2424 <guillaume.dib@gmail.com>
2025-02-23 15:43:08 -0500

    Revert back nrf24l01 from lib to application for now

--- bsp/PRoMo_v0_1/spi.h
+++ bsp/PRoMo_v0_1/spi.h
@@ -6,15 +6,19 @@
 #include <stddef.h>
 #include "bsp.h"
 
-#define spi2_init() 							spi_init(SPI2)
-#define spi2_send_byte_waiting(write_data, read_data) \
-												spi_send_byte_waiting(SPI2, write_data, read_data)
-#define spi2_send_multiple_bytes_waiting(write_data, read_data, size) \
-												spi_send_multiple_bytes_waiting(SPI2, write_data, read_data, size)
-
 
 extern void spi_init(SPI_TypeDef *SPIx);
 extern bool spi_send_byte_waiting(SPI_TypeDef *SPIx, uint8_t write_data, uint8_t *read_data);
 extern bool spi_send_multiple_bytes_waiting(SPI_TypeDef *SPIx, uint8_t* write_data, uint8_t* read_data, uint8_t size);
+
+static inline void spi2_init(void) { 
+    spi_init(SPI2); 
+}
+static inline bool spi2_send_byte_waiting(uint8_t write_data, uint8_t *read_data) { 
+    return spi_send_byte_waiting(SPI2, write_data, read_data); 
+}
+static inline bool spi2_send_multiple_bytes_waiting(uint8_t *write_data, uint8_t *read_data, uint8_t size) { 
+    return spi_send_multiple_bytes_waiting(SPI2, write_data, read_data, size); 
+}
 
 #endif
